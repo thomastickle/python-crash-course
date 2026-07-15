ae870b3ce01ee4ec915486b7737606cb94072514
Thomas Tickle <ttickle@gmail.com>
2026-07-14 20:11:31 -0400

    Finished chapter 3.

--- chapter2/chapter_2.py
+++ chapter2/chapter_2.py
@@ -49,3 +49,9 @@ print(24 / 3)
 # 2-10
 favorite_number = 21
 print(f"My favorite number is {favorite_number}.")
+
+# 2-11
+# This was supposed to be done on an older file, but since I have had them all in here,
+# we will just comment here.
+# Thomas Tickle
+# This does some exploration with strings and string operations along with some with math.

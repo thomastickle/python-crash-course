4ef991c25f4ef7fb5a7c022e9f6fd772a7a63f4c
Thomas Tickle <ttickle@gmail.com>
2026-07-20 22:33:26 -0400

    Chapter 6 is now complete.

---
+++ chapter6/exercise_6_12.py
@@ -0,0 +1,8 @@
+from util.exercise_output import print_exercise_header
+
+
+print_exercise_header("6-12 Extensions")
+
+print(
+    "Not doing anything here because we were doing much of it already as Iworked on it."
+)

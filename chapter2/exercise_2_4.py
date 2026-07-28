977b76fe8817685f44f03bb62db9e215c7ffa5eb
Thomas Tickle <ttickle@gmail.com>
2026-07-28 03:27:33 -0400

    Fixed a minor typo.

--- chapter2/exercise_2_4.py
+++ chapter2/exercise_2_4.py
@@ -1,8 +1,7 @@
 from util.exercise_output import print_exercise_header
 
-
 print_exercise_header("2-4")
-name = "thomas tickler"
+name = "thomas tickle"
 print(f"{name.lower()}")
 print(f"{name.upper()}")
 print(f"{name.title()}")
